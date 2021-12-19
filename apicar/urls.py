ad08447c8a4f216724cecfcc42c03de1b876a215
sspakuha <73202124+sspakuha@users.noreply.github.com>
2021-12-19 23:31:46 +0100

    urls fixed

--- apicar/urls.py
+++ apicar/urls.py
@@ -3,8 +3,8 @@ from apicar import views
 from django.urls import path, include
  
 urlpatterns = [ 
-    path('cars', views.cars, name="cars"),
+    path('cars/', views.cars, name="cars"),
 	 path('cars/<int:id>', views.delete, name="delete"),
-	 path('rate', views.rate, name="rate"),
-	 path('popular', views.popular, name="popular")
+	 path('rate/', views.rate, name="rate"),
+	 path('popular/', views.popular, name="popular")
 ]
